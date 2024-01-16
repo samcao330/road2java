910e818e30066a806cff6f268404477dd69533d8
beishan gongyuan <beishangongyuan@beishans-MacBook-Air-2.local>
2024-01-16 12:30:20 +0800

    try again

---
+++ Test/src/Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int x;
+        int a, b, c, d, e;
+        double y = 1.1;
+        System.out.println(y);
+        x = 3;
+        y = x +y;
+
+    }
+}
