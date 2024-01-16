1fb146d52284bc051cb9e7f0ee228efebaf487b7
samcao330 <beishan2022@gmail.com>
2024-01-17 07:57:16 +0800

    Jan 16, 2024

--- Jan16/src/Jan16.java
+++ Jan16/src/Jan16.java
@@ -1,13 +1,47 @@
 public class Jan16 {
     public static void main(String[] args) {
-        int num = 1;
-        while(num <= 100){
-            if(num % 15 != 0){
-                num++;
-                continue;
-            }
-            System.out.println(num);
-            num++;
+        int sum = 0;
+        int product = 1;
+        for(int num = 1; num <= 5; num++){
+            product = product * num;
+            sum = sum + product;
         }
+        System.out.println(sum);
     }
 }
+//    public static void main(String[] args) {
+//        int product = 1;
+//        for(int num = 1; num <= 5; num++){
+//           product = product * num;
+//        }
+//        System.out.println(product);
+//    }
+//}
+//    public static void main(String[] args) {
+//        int sum = 0;
+//        for(int num = 1; num <= 100; num++){
+//            sum = sum + num;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+//    public static void main(String[] args) {
+//        for (int num = 1; num <= 10; num++) {
+//            System.out.println(num);
+//        }
+//    }
+//}
+
+    //    public static void main(String[] args) {
+//        int num = 1;
+//        while(num <= 100){
+//            if(num % 15 != 0){
+//                num++;
+//                continue;
+//            }
+//            System.out.println(num);
+//            num++;
+//        }
+//    }
+//}
+
