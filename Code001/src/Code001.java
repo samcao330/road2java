bb093a8e56290ba8d0232540e8a28b1abca9ea04
samcao330 <beishan2022@gmail.com>
2024-01-17 08:39:57 +0800

    Jan 17, 2024

--- Code001/src/Code001.java
+++ Code001/src/Code001.java
@@ -1,2 +1,29 @@
 public class Code001 {
-}
+    //    public static void main(String[] args) {
+//        int sum = 0;
+//        for(int num = 1; num <= 5; num++){
+//            for(int proVar = 1; proVar <= num; proVar++){
+//                int product = 1;
+//                product = product*proVar;
+//                sum = sum + product;
+//            }
+//        }
+//        System.out.println(sum);
+//    }
+//}
+//    public static void main(String[] args) {
+//        int num = 1;
+//        int sum = 0;
+//        while(num <= 5){
+//            int proVar = 1;
+//            int product = 1;
+//            while(proVar <= num){
+//                product = product * proVar;
+//                proVar++;
+//            }
+//            sum = sum + product;
+//            num++;
+//        }
+//        System.out.println(sum);
+//    }
+//}
